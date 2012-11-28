67b743a1a51777922747c2092c6b6b7fe637f0c2
salini <josephsalini@gmail.com>
2012-11-28 21:15:33 +0100

    improve joint limit constraint, by taking into account time of inflexion

--- LQPctrl/constraints.py
+++ LQPctrl/constraints.py
@@ -9,7 +9,7 @@ Each constraint is described by the couples:
     (G,h) for ineqality constraints such as Gx <= h
 """
 from numpy import zeros, array, arange, dot, hstack, vstack, fmin, fmax, \
-                  isnan, cos, sin, pi
+                  isnan, cos, sin, pi, nan, isfinite, isinf
 
 def eq_motion(M, Jchi_T, G_N):
     """ equation of motion: M.dgvel + N = S.gforce + Jc_T.fc + G
@@ -77,7 +77,7 @@ def ineq_gforcemax(gforcemax, dgforcemax, dt, gforce_prec, \
     """ inequality of gforcemax: B_min <= gforce <= B_max
     with B_min = max(-gforcemax, gforce_prec - dgforcemax*dt)
          B_max = min( gforcemax, gforce_prec + dgforcemax*dt)
-    
+
     for formalism 'dgvel chi':
     Rewrite:         |dgvel |
                      |fc    |
@@ -163,12 +163,13 @@ def ineq_friction(mus, const_activity, n_pan, n_dof, \
 
 
 
+
 def ineq_joint_limits(qlim, vlim, gpos, gvel, hpos, hvel, n_problem, \
                       formalism='dgvel chi', Minv_Jchi_T=None, Minv_G_N=None):
     """ inequality of joint limits: B_min <= K.dgvel <= B_max
     with
-    B_min = max(2*(pos_lim_dn - pos -hpos.gvel)/hpos**2, (-vel_max - gvel)/hvel)
-    B_max = min(2*(pos_lim_up - pos -hpos.gvel)/hpos**2, ( vel_max - gvel)/hvel)
+    B_min = max(2*(pos_lim_dn - pos -hpos.gvel)/hpos**2, (-vel_max - gvel)/hvel, gvel**2/(2*(gpos - pos_lim_dn)).Cond(tmin>0) )
+    B_max = min(2*(pos_lim_up - pos -hpos.gvel)/hpos**2, ( vel_max - gvel)/hvel, gvel**2/(2*(gpos - pos_lim_up)).Cond(tmax>0) )
 
     for formalism 'dgvel chi':
     Rewrite:         |dgvel |
@@ -181,20 +182,55 @@ def ineq_joint_limits(qlim, vlim, gpos, gvel, hpos, hvel, n_problem, \
            [ K]Minv[Jc_T, S]|gforce|    | B_max| - [ K]Minv[G-N]
            [-K]                      <= |-B_min|   [-K]
     """
-    B_min = fmax((-vlim - gvel)/hvel, 2*(qlim[:, 0] - gpos - hpos*gvel)/hpos**2)
-    B_max = fmin(( vlim - gvel)/hvel, 2*(qlim[:, 1] - gpos - hpos*gvel)/hpos**2)
+    
+    ##### Compute bounds for velocity limits considering horizon position #####
+    bmin_vel = (-vlim - gvel)/hvel
+    bmax_vel = ( vlim - gvel)/hvel
+    
+    ##### Compute bounds for position limits considering horizon position #####
+    bmin_pos = 2*(qlim[:, 0] - gpos - hpos*gvel)/hpos**2
+    bmax_pos = 2*(qlim[:, 1] - gpos - hpos*gvel)/hpos**2
+    
+    ##### Compute bounds for position limits considering time of inflexion #####
+    tmin = -2*(gpos - qlim[:, 0]) / gvel
+    tmax = -2*(gpos - qlim[:, 1]) / gvel
 
-    selected_dof = [i for i in arange(len(B_min)) if not isnan(B_min[i])]
-    n_lim_dof = len(selected_dof)
+    tmin[tmin <=  0  ] = nan
+    tmin[tmin > hpos] = nan
+    tmin[isfinite(tmin)] = 1.
+    tmax[tmax <=  0  ] = nan
+    tmax[tmax > hpos] = nan
+    tmax[isfinite(tmax)] = 1.
 
-    K = zeros((2*n_lim_dof, len(B_min)))
-    K[arange(n_lim_dof), selected_dof] = 1
-    K[arange(n_lim_dof, 2*n_lim_dof), selected_dof] = -1
+    bmin_pos_time = gvel**2/(2*(gpos - qlim[:, 0])) * tmin
+    bmax_pos_time = gvel**2/(2*(gpos - qlim[:, 1])) * tmax
 
-    h = hstack((B_max[selected_dof], -B_min[selected_dof]))
+    ##### Concatenate bounds #####
+    B_min = bmin_vel
+    B_max = bmax_vel
+    B_min = fmax(B_min, bmin_pos)
+    B_max = fmin(B_max, bmax_pos)
+    B_min = fmax(B_min, bmin_pos_time)
+    B_max = fmin(B_max, bmax_pos_time)
 
+    ##### Create K, concatenation of B_max & B_min #####
+    smax_dof = [i for i in arange(len(B_max)) if not isnan(B_max[i])]
+    smin_dof = [i for i in arange(len(B_min)) if not isnan(B_min[i])]
+    
+    nmax = len(smax_dof)
+    nmin = len(smin_dof)
+    K = zeros((nmax+nmin, len(B_min)))
+    
+    K[arange(nmax),            smax_dof] =  1
+    K[arange(nmax, nmax+nmin), smin_dof] = -1
+
+    ##### Create G & h, depending on formalism #####
+    h = zeros(nmax+nmin)
+    h[:nmax] =  B_max[smax_dof]
+    h[nmax:] = -B_min[smin_dof]
+    
     if formalism == 'dgvel chi':
-        G = zeros((2*n_lim_dof, n_problem))
+        G = zeros((len(K), n_problem))
         G[:, :len(B_min)] = K
     elif formalism == 'chi':
         G = dot(K, Minv_Jchi_T)
@@ -219,6 +255,7 @@ def ineq_collision_avoidance(sdist, svel, J, dJ, gvel, \
     Rewrite:                |fc    |
            [ K]Minv[Jc_T, S]|gforce| <= | B | - [ K]Minv[G-N]
     """
+    #TODO: complete this function to take into account time of inflexion
     B = 2*(sdist + hpos*svel)/hpos**2 + dot(dJ, gvel)
     K = -J
 
